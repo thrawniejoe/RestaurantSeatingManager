d16f0aaa058304ddf063b8b970289821cfa58f6a
John_Soerjawitaka <johnsoerjawitaka@yahoo.com>
2017-08-23 12:48:10 -0500

    added splash login

---
+++ SeatingManager/SplashLogin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeatingManager
+{
+    /// <summary>
+    /// Interaction logic for SplashLogin.xaml
+    /// </summary>
+    public partial class SplashLogin : Window
+    {
+        MainWindow main = new MainWindow();
+        public SplashLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Move(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text.Equals("john") && txtPassword.Password.Equals("password"))
+            {
+                main.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username and/or Password. Please try again.");
+            }
+        }
+    }
+}
