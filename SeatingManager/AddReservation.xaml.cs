fb51c1a1c385d91a12b5efe0d18dc0e71e6de350
John_Soerjawitaka <johnsoerjawitaka@yahoo.com>
2017-09-14 15:46:26 -0500

    added addreservation page

---
+++ SeatingManager/AddReservation.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeatingManager
+{
+    /// <summary>
+    /// Interaction logic for AddReservation.xaml
+    /// </summary>
+    public partial class AddReservation : Window
+    {
+        public AddReservation()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            SeatingManager.SeatingManagerDBDataSet seatingManagerDBDataSet = ((SeatingManager.SeatingManagerDBDataSet)(this.FindResource("seatingManagerDBDataSet")));
+            // Load data into the table customers. You can modify this code as needed.
+            SeatingManager.SeatingManagerDBDataSetTableAdapters.customersTableAdapter seatingManagerDBDataSetcustomersTableAdapter = new SeatingManager.SeatingManagerDBDataSetTableAdapters.customersTableAdapter();
+            seatingManagerDBDataSetcustomersTableAdapter.Fill(seatingManagerDBDataSet.customers);
+            System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
+            customersViewSource.View.MoveCurrentToFirst();
+        }
+    }
+}
